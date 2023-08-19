6f5a90bb4f4430c5800f7d11c7b082f82a8ed3ab
Anton Vashenko <vashchenko-96@mail.ru>
2023-08-19 12:14:54 +0300

    Project and app was renamed.

--- homework_01/homework_01_poject/wsgi.py
+++ homework_01/homework_01_poject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for homework_01 project.
+WSGI config for homework_01_poject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homework_01.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homework_01_poject.settings')
 
 application = get_wsgi_application()
--- homework_01/task_1_app/apps.py
+++ homework_01/task_1_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class Task1Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'task_1'
+    name = 'task_1_app'
